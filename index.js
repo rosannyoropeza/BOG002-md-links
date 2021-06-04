fef32fadb113fbb0b28949b36639fce926156071
rosannyoropeza <rosanny.oropeza82@gmail.com>
2021-06-04 15:44:17 -0500

    Captura de parámetro enviado desde la terminal de Node

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+// Enviar Parámetros desde la terminal (Node)
+const param1 = process.argv[0]; // PARA CAPTURAR NODE
+const param2 = process.argv[1]; // PARA CAPTURAR LA RUTA ABSOLUTA DEL ARCHIVO QUE SE EJECUTA
+let param3 = process.argv[2]; // PARA 1ER PARAMETRO
+let param4 = process.argv[3]; // PARA 2DO PARAMETRO
+
+console.log(param1, param2, param3, param4)
