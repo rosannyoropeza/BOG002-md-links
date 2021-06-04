3dce4955d080a9c4ff46d7615da29c81924210f4
rosannyoropeza <rosanny.oropeza82@gmail.com>
2021-06-04 16:06:57 -0500

    Covirtiendo la ruta ingresada en caso que sea una ruta  relativa a una ruta absoluta

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //   // ...
 // };
 
+const path = require('path');
+
 // Enviar Parámetros desde la terminal (Node)
 const param1 = process.argv[0]; // PARA CAPTURAR NODE
 const param2 = process.argv[1]; // PARA CAPTURAR LA RUTA ABSOLUTA DEL ARCHIVO QUE SE EJECUTA
@@ -9,3 +11,10 @@ let param3 = process.argv[2]; // PARA 1ER PARAMETRO
 let param4 = process.argv[3]; // PARA 2DO PARAMETRO
 
 console.log(param1, param2, param3, param4)
+
+// Convirtiendo una ruta relativa a ruta absoluta
+if (path.isAbsolute(param3) === false){
+  param3 = path.resolve(param3)
+}
+
+console.log("soy ruta convertida", param3)
