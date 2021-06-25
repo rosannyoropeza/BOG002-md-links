170f2c762ec774c394ec26ae28f32303b99a21e4
rosannyoropeza <rosanny.oropeza82@gmail.com>
2021-06-24 20:14:24 -0500

    Crear opción validate, stats, help

--- index.js
+++ index.js
@@ -1,110 +1,107 @@
-// module.exports = () => {
-//   // ...
-// };
-
+#!/usr/bin/env node
+// const mdLinks = require("./md-Links");
 const path = require('path');
-const fs = require('fs');
-const MarkdownIt = require('markdown-it');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-var http = require('http');
-var https = require('https');
-const { resolve } = require('path');
+const chalk = require('chalk');
+const mdLinks = require("./duplicate");
 
-// Enviar Parámetros desde la terminal (Node)
-const param1 = process.argv[0]; // PARA CAPTURAR NODE
-const param2 = process.argv[1]; // PARA CAPTURAR LA RUTA ABSOLUTA DEL ARCHIVO QUE SE EJECUTA
-let param3 = process.argv[2]; // PARA 1ER PARAMETRO
-let param4 = process.argv[3]; // PARA 2DO PARAMETRO
+let pathFile = process.argv[2]; // PARA 1ER PARAMETRO
+let validate = false;
+let help = false;
+let stats = false;
 
-// Convirtiendo una ruta relativa a ruta absoluta
-if (path.isAbsolute(param3) === false) {
-  param3 = path.resolve(param3)
-}
-
-// Verificando si es un file o un directorio
-let arraydir = [];
-
-function GetFiles(param3) {
-  statsDirectory = fs.lstatSync(param3).isDirectory();
-  if (statsDirectory) {
-    const directory = fs.readdirSync(param3)
-    for (let index in directory) {
-      let nextDirectory = path.join(param3, directory[index]);
-      if (path.extname(directory[index]) === ".md") {
-        arraydir.push(param3 + "\\" + directory[index]);
-      }
-      if (fs.lstatSync(nextDirectory).isDirectory()) {
-        GetFiles(nextDirectory)
-      }
-    }
-    return arraydir;
+const params = process.argv; // PARA 2DO y 3ER PARAMETRO
+params.forEach((param) => {
+  if (param === '--validate') {
+    validate = true;
   }
-  if (path.extname(param3) === ".md") {
-    arraydir.push(param3);
-    return arraydir;
+  if (param === '--help') {
+    help = true;
   }
-}
-
-const arraydir2 = GetFiles(param3)
-// console.log("soy directorio final", arraydir2)
-
-// Para leer el contenido del archivo recibido mediante la ruta
-function readFileContent(arraydir) {
-  const md = new MarkdownIt();
-  let result;
-  const dataLinks = [];
-  arraydir.forEach(file => {
-    const data = fs.readFileSync(file, { encoding: 'utf8' })
-    result = md.render(data);
-    const dom = new JSDOM(result);
-    let Nodelinks = dom.window.document.querySelectorAll("a");
-    Nodelinks.forEach((link) => {
-      if (link.href.startsWith("http")) {
-        dataLinks.push({
-          href: link.href,
-          text: link.textContent,
-          file: file,
-        })
-        // return dataLinks
-      }
-    })
-  });
-  return dataLinks;
-}
-
-const fileContent = readFileContent(arraydir2)
-// console.log("soy lectura de archivo", fileContent)
-
-function validateStatusLinks(fileContent) {
-  const newArrObj = [];
-  fileContent.forEach((link) => {
-    if (link.href.startsWith('https')) {
-      const httpsGet = new Promise(function(resolver, rechazar){
-        https.get(link.href, function (res) {
-          const newObj = {
-            href: link.href,
-            text: link.text,
-            file: link.file,
-            status: res.statusCode,
-            ok: res.statusMessage,
-          }
-          res.on('error', function (e) {
-            console.error(e);
-          });
-          // console.log(newObj)
-          resolver (newObj);
-        })
-      });
-      newArrObj.push(httpsGet)
-    }
-  });
-  return Promise.all(newArrObj);
-  
-}
-
-validateStatusLinks(fileContent).then((resul) => {
-  console.log(resul)
+  if (param === '--stats') {
+    stats = true;
+  }
 })
 
+options = {
+  'validate': validate,
+  'stats': stats,
+  'help': help,
+}
+
+if (help)
+{
+  //MUESTRAR TABLA INFORMATIVA
+  const helps = {
+    "--validate": "Muestra los links validados",
+    "--stats": "Muestra el total de links y cuantos son únicos",
+    "--stats --validate": "Valida los links y muestra las estadística"
+  }
+
+  console.table(helps);
+} 
+else 
+{
+  if (stats && validate) 
+  {
+    //MOSTRAR ESTADISTICAS Y VALIDACION 
+    mdLinks.main(pathFile, { validate: true }).then((res) => {
+      //TOTAL
+      console.log("Total: ", res.length)
+
+      //UNICOS
+      const arrayLinks =  res.map((link) => { return link.href } )
+      const linksUnicos =  arrayLinks.filter((link, index, array) => { return array.indexOf(link) === index; } )
+      console.log("Unique: ", linksUnicos.length)
+
+      //ROTOS
+      const arrayLinkStatus =  res.map((link) => { return link.ok })
+      console.log("soy fail", arrayLinkStatus)
+      const linksRotos =  arrayLinkStatus.filter((link) => { 
+        if(link === 'Fail')
+       return link;
+      });
+      console.log("Broken: ", linksRotos.length);
+    });
+  } 
+  else 
+  {
+    if (stats) 
+    {
+      //MOSTRAR ESTADISTICAS 
+      mdLinks.main(pathFile, { validate: true }).then((res) => {
+        //TOTAL
+        console.log("Total: ", res.length);
+
+        //UNICOS
+        const arrayLinks =  res.map((link) => { return link.href });
+        const linksUnicos =  arrayLinks.filter((link, index, array) => { return array.indexOf(link) === index; });
+        console.log("Unique: ", linksUnicos.length);
+      });
+    } 
+    else 
+    {
+      //LINKS VALIDADOS
+      if (validate)
+      {
+        mdLinks.main(pathFile, { validate: true }).then((res) => {
+          res.forEach(link => {
+            const fileName = path.parse(link.file);
+            console.log(chalk.blue(fileName.base), chalk.green(link.href), chalk.yellow(link.status), chalk.red(link.ok), chalk.magenta(link.text));
+          });
+        });
+      }
+      else 
+      {
+        //LINKS SIN VALIDAR
+        mdLinks.main(pathFile, options).then((res) => {
+          res.forEach(link => {
+            const fileName = path.parse(link.file);
+            console.log(chalk.blue(fileName.base), chalk.green(link.href), chalk.magenta(link.text));
+          });
+        });
+      }
+    }
+
+  }
+}
 
