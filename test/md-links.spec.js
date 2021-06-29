483309060ab792271498704badf34dd8ef135690
rosannyoropeza <rosanny.oropeza82@gmail.com>
2021-06-29 18:06:37 -0500

    Test de funcion mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,8 @@
+const { validateStatusLinks } = require('../md-Links');
 const mdLinks = require('../md-Links');
 const file = '../PRUEBA_TEST/dir_1/file1.md';
 const dir = './PRUEBA_TEST'
-
+const file5 = '../PRUEBA_TEST/dir_2/file5.md';
 const arrayObj = [
     {
         href: "https://google.com/",
@@ -18,25 +19,22 @@ const arrayValidado = [{
 }]
 
 describe('mdLinks.main', () => {
-    jest.setTimeout(50000);
+    jest.setTimeout(200000);
     it('Deberia retornar una ruta absoluta', () => {
         expect(typeof mdLinks.main('README.md', { validate: true })).toBe("object");
     });
-    it('Deberia indicar si el archivo no existe', () => {
-        expect(mdLinks.main("1234.md", { validate: true })).toBeTruthy();
+    it('Deberia retornar un string El archivo ¡NO EXISTE!', () => {
+        return mdLinks.main("1234.md", { validate: true }).catch((error) => {
+            expect(error).toEqual("El archivo ¡NO EXISTE!");
+        });
     });
     it('Deberia devolver un array de archivos .md', () => {
-        const directory = ["C:\\Users\\romar\\OneDrive\\Documents\\BOG002-md-links\\README.md", "PRUEBA_TEST\\dir_1\\file1.md", "PRUEBA_TEST\\dir_2\\file2.md", "PRUEBA_TEST\\dir_2\\file3.md"];
+        const directory = ["C:\\Users\\romar\\OneDrive\\Documents\\BOG002-md-links\\README.md", "PRUEBA_TEST\\dir_1\\file1.md", "PRUEBA_TEST\\dir_2\\file2.md", "PRUEBA_TEST\\dir_2\\file3.md", "PRUEBA_TEST\\dir_2\\file4.md", "PRUEBA_TEST\\dir_2\\file5.md"];
         expect(mdLinks.getFiles(dir)).toEqual(directory);
     });
     it('Deberia retornar un array de objetos al realizar una petición HTTP', () => {
         return mdLinks.validateStatusLinks(arrayObj).then((res) => {
             expect(res).toEqual(arrayValidado);
-        })
+        });
     });
-    it('Deberia retornar un array de objetos sin validar', () => {
-        return mdLinks.main(arrayObj).then((res) => {
-            expect(res).toEqual(arrayObj);
-        })
-    },1000);
-});
+})
